a2a1800fddffb9c555e27da23e5f37ee50216318
Amnat Chattamanokul <49928239+darkmancer@users.noreply.github.com>
2021-05-27 19:55:57 +0700

    Delete TDD directory

--- TDD/one-tdd/test/my-test-test.js
+++
@@ -1,9 +0,0 @@
-const { add , multi } = require('../my-test')
-
-test('two plus two is four', () => {
-    expect(add(2, 2)).toBe(4);
-});
-
-test('two multiple two is four', () => {
-    expect(multi(2, 3)).toBe(6);
-});
